579cfc0f26cf856fc4be0f0ba51b0a37cf943ba3
Ben Welland <benjamin.welland@student.manchester.ac.uk>
2021-11-30 14:51:49 +0000

    Added the handler events to the sleep handler file.

--- src/games/stendhal/server/entity/status/SleepStatusHandler.java
+++ src/games/stendhal/server/entity/status/SleepStatusHandler.java
@@ -1,21 +1,46 @@
 package games.stendhal.server.entity.status;
 
+import games.stendhal.common.NotificationType;
+import games.stendhal.server.core.events.TurnNotifier;
 import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.RPEntity;
 import games.stendhal.server.entity.player.Player;
+import java.lang.Math;
 
 public class SleepStatusHandler implements StatusHandler<SleepStatus>{
 
 	@Override
 	public void inflict(SleepStatus status, StatusList statusList, Entity bed) {
 		// TODO Auto-generated method stub
+		if (statusList.hasStatus(status.getStatusType())) {
+			return;
+		}
 		Player sleeper = (Player) statusList.getEntity();
-		sleeper.setSpeed(0);
+		if(sleeper == null) {
+			return;
+		}
+		if(Math.abs(bed.getX() - sleeper.getX()) < 2 && Math.abs(bed.getY() - sleeper.getY()) < 2) {
+			sleeper.setSpeed(0);
+		} else {
+			return;
+		}
+		sleeper.sendPrivateText(NotificationType.SCENE_SETTING, "You are sleeping.");
+		statusList.activateStatusAttribute("status_" + status.getName());
+		statusList.addInternal(status);
+		TurnNotifier.get().notifyInSeconds(60, new StatusRemover(statusList, status));
 	}
 
 	@Override
 	public void remove(SleepStatus status, StatusList statusList) {
-		// TODO Auto-generated method stub
-		
+		statusList.removeInternal(status);
+
+		RPEntity entity = statusList.getEntity();
+		if (entity == null) {
+			return;
+		}
+
+		entity.sendPrivateText(NotificationType.SCENE_SETTING, "You are no longer sleeping.");
+		entity.remove("status_" + status.getName());		
 	}
 
 }
